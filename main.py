b436b72abc0e2ef754b529e294eb238ff4551234
Assalahu <dla.cruz2002@gmail.com>
2020-12-22 20:42:28 +0100

    addddddddddd
    add stuf y cosas q algunas no funcionan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyautogui
 import time
+import pyperclip
+from tkinter import *
+from tkinter import messagebox as MessageBox
 
 def isPrime(num):
     if num < 1:
@@ -12,13 +15,13 @@ def isPrime(num):
                 return False
         return True
 
-
+time.sleep(3)
 opciones = pyautogui.prompt(' [0] Meter cosas randoms\n [1] Meter textos')
 opciones = int(opciones)
 time.sleep(0.5)
 i = 1
 if opciones == 1:
-    eleccion = pyautogui.prompt(' [0] Bee movie\n [1] Mistborn\n [2] Shrek\n [3] Somebody\n [4] Otra\n [5] Otra')
+    eleccion = pyautogui.prompt(' [0] Bee movie\n [1] Mistborn\n [2] Shrek\n [3] Somebody\n [4] Miku\n [5] Kaladin siendo un dios')
     eleccion = int(eleccion)
     time.sleep(2.5)
 
@@ -31,14 +34,16 @@ if opciones == 1:
     if eleccion == 3:
         f = open("somebody.txt", 'r')
     if eleccion == 4:
-        while i != 10000:
-            pyautogui.press('enter')
-            i = i + 1
+        f = open("miku.txt", 'r')
+    if eleccion == 5:
+        f = open("honor.txt", 'r')
     for word in f:
-        pyautogui.typewrite(word)
+        if f != "" or f != " ":
+            pyautogui.typewrite(word)
+
 
 elif opciones == 0:
-    eleccion = pyautogui.prompt(' [0] Contar\n [1] Espacios\n [2] Intos \n [3] Numeros primos\n [4] Otra\n [5] Otra')
+    eleccion = pyautogui.prompt(' [0] Contar\n [1] Espacios\n [2] Intos \n [3] Numeros primos\n [4] Donut\n [5] Putx')
     eleccion = int(eleccion)
     time.sleep(2.5)
     if eleccion == 0:
@@ -62,7 +67,26 @@ elif opciones == 0:
         a = 1
         while a != 1000000000:
             a = a + 1
-            if isPrime(a) == True:
+            if isPrime(a) is True:
                 A = str(a)
                 pyautogui.typewrite(A)
                 pyautogui.press('enter')
+
+    if eleccion == 4:
+        while i != 100000000:
+            pyautogui.write('.')
+            f = open("donut.txt", 'r')
+            for word in f:
+                pyautogui.write(".")
+                pyperclip.copy(word)
+                pyautogui.hotkey('ctrl', 'v', interval=0.001)
+                pyautogui.press('enter')
+
+    if eleccion == 5:
+        puto = 3
+        while puto <= 1000000:
+            pyautogui.press('n')
+            pyautogui.write("F")
+            pyautogui.hotkey('ctrl', 'Enter', interval=.1)
+
+
